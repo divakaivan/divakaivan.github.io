a9c8440ace3a9e18af213020bc00afbf19502c28
Ivan Ivanov <54508530+divakaivan@users.noreply.github.com>
2026-04-01 23:20:45 +0100

    Update whoami.ts

--- lib/content/whoami.ts
+++ lib/content/whoami.ts
@@ -8,7 +8,7 @@ Github - https://github.com/divakaivan
 
 In 2024, I studies and wrote a blog every day - check \`cd 2024/\` and then \`less 2025-01-01-the-final-day-366-this-blog-ends-but-my-learning-journey-continues.md\` or any other day
 
-In 2025 and 2026, I switch to monthly blogs on a single page - check \`less 2025.md/\` and \`less 2026.md/\`
+In 2025 and 2026, I switch to monthly blogs on a single page - check \`less 2025.md\` and \`less 2026.md\`
 
 ---
 
