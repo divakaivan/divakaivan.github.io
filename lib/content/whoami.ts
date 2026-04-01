1452cd08d3b2f4f55a3862ef8b422dc2b29f96c6
Ivan Ivanov <54508530+divakaivan@users.noreply.github.com>
2026-04-01 23:16:43 +0100

    Update whoami.ts

--- lib/content/whoami.ts
+++ lib/content/whoami.ts
@@ -1,6 +1,12 @@
-const whoami = `# Your name
+const whoami = `# Ivan Ivanov
 
-Tp update this write markdown in \`lib/content/whoami.ts\`
+I use this space to share what I do outside of work
+
+LinkedIn - https://www.linkedin.com/in/ivansivanov/
+Github - https://github.com/divakaivan
+
+In 2024, I studies and wrote a blog every day - check \`cd 2024/\` and then \`less 2025-01-01-the-final-day-366-this-blog-ends-but-my-learning-journey-continues.md\` or any other day
+In 2025 and 2026, I switch to monthly blogs on a single page.
 
 ---
 
