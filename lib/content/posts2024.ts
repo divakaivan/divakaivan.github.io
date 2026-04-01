a8b4ea555f49eaefbe63ca3444a0857a4c662f0f
divakaivan <timee98@outlook.com>
2026-04-01 21:12:48 +0100

    posts

--- lib/content/posts2024.ts
+++ lib/content/posts2024.ts
@@ -1,9 +1,12 @@
-import buildingDataPipeline from "../../content/posts/2024/building-a-data-pipeline.md";
-import lessonsFromOpenSource from "../../content/posts/2024/lessons-from-open-source.md";
+declare const require: any;
 
-const posts2024: Record<string, string> = {
-  "building-a-data-pipeline.md": buildingDataPipeline,
-  "lessons-from-open-source.md": lessonsFromOpenSource,
-};
+const context = require.context("../../content/posts/2024", false, /\.md$/);
+
+const posts2024: Record<string, string> = {};
+
+context.keys().forEach((key: string) => {
+  const fileName = key.replace("./", "");
+  posts2024[fileName] = context(key).default || context(key);
+});
 
 export default posts2024;
